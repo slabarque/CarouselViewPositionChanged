d1ef8af1630836d13c90869d0b2371f7a54ff54c
Stif <stiflabarque@fisheye.eu>
2025-07-26 11:01:23 +0200

    dotnet new maui

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CarouselViewPositionChanged;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CarouselViewPositionChanged;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
